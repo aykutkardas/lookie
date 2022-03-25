da86f5df89d3ff9cbe6ea0daf40059b8ec16ea3e
Aykut Kardaş <aykutkrds@gmail.com>
2022-03-25 19:31:56 +0300

    Refactor codebase

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,4 @@
-type MSTimeTable = {
-  Y?: number;
-  M?: number;
-  D?: number;
-  h?: number;
-  m?: number;
-  s?: number;
-};
-
-type ExpiryTime = number | string | MSTimeTable;
-
-type Data = { value: any; expiry?: number };
-
-export const MS_TIME_TABLE: MSTimeTable = {
+export const MS_TIME_TABLE = {
   Y: 31556926000,
   M: 2629743830,
   D: 86400000,
@@ -19,6 +6,12 @@ export const MS_TIME_TABLE: MSTimeTable = {
   m: 60000,
   s: 1000,
 };
+
+type MSTimeTable = typeof MS_TIME_TABLE;
+
+type ExpiryTime = number | string | MSTimeTable;
+
+type Data = { value: any; expiry?: number };
 
 export const convertObjToMS = (timeObj: MSTimeTable): number => {
   let totalTime = 0;
@@ -38,22 +31,23 @@ export const convertStringToMS = (timeStr: string): number => {
   const timeArr = timeStr.split(" ");
   let totalTime = 0;
 
-  Object.keys(MS_TIME_TABLE).forEach((key) => {
-    timeArr.forEach((time) => {
-      if (time.indexOf(key) > -1 && parseInt(time)) {
-        totalTime = parseInt(time) * MS_TIME_TABLE[key];
-      }
-    });
+  timeArr.forEach((time) => {
+    const key = /[YMDhms]/g.exec(time);
+    const parsedTime = parseInt(time);
+
+    if (!key || !parsedTime) return;
+
+    const currentTime = MS_TIME_TABLE[key[0]];
+
+    totalTime = parseInt(time) * currentTime;
   });
 
   return totalTime;
 };
 
 class Lookie {
-  static set = (key: string, value: any, expiryTime?: ExpiryTime): void => {
-    if (!key || typeof value === "undefined") {
-      return;
-    }
+  static set = (key: string, value: any, expiryTime?: ExpiryTime) => {
+    if (!key || typeof value === "undefined") return;
 
     let expiryTimeMs = 0;
 
@@ -77,7 +71,7 @@ class Lookie {
     localStorage.setItem(key, JSON.stringify(data));
   };
 
-  static setAll = (obj: Object, expiryTime?: ExpiryTime): void => {
+  static setAll = (obj: Object, expiryTime?: ExpiryTime) => {
     if (typeof obj !== "object") return;
 
     Object.entries(obj).forEach(([key, value]) => {
@@ -88,19 +82,15 @@ class Lookie {
   static get = (key: string): any => {
     const dataStr = localStorage.getItem(key);
 
-    if (!dataStr) {
-      return null;
-    }
+    if (!dataStr) return null;
 
     try {
       const item = JSON.parse(dataStr);
       const now = new Date().getTime();
 
-      if (item.expiry) {
-        if (now > item.expiry) {
-          localStorage.removeItem(key);
-          return null;
-        }
+      if (item.expiry && now > item.expiry) {
+        localStorage.removeItem(key);
+        return null;
       }
 
       return item.value;
@@ -109,12 +99,12 @@ class Lookie {
     }
   };
 
-  static remove = (key: string): void => {
+  static remove = (key: string) => {
     localStorage.removeItem(key);
   };
 
-  static sync = (): void => {
-    Object.keys(localStorage).forEach((key) => Lookie.get(key));
+  static sync = () => {
+    Object.keys(localStorage).forEach(Lookie.get);
   };
 }
 
